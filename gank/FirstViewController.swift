56a1889684cc62b69877ae5987d9b1cc741fea05
pkhope <hhxnbw@163.com>
2016-12-10 17:18:52 +0800

    Initial Commit

---
+++ gank/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  gank
+//
+//  Created by huhexiang on 16/12/10.
+//  Copyright © 2016年 huhexiang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
